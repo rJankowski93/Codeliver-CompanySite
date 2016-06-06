6028e74025ce7094140556ee41b9b1dbb8e67a9f
Rafal Jankowski <rafal.jankowski.93@gmail.com>
2016-06-06 23:14:06 +0200

    fix logo and menu

--- js/script.js
+++ js/script.js
@@ -1,42 +1,126 @@
-var slowScroll = (function() {
-	
+var all = (function() {
+	var variables = {
+		sectionaAboutUs : {},
+		sectionWhatDo : {},
+		sectiontTechnology : {},
+		sectionContact : {},
+		positionSectionAboutUs : {},
+		positionSectionWhatDo : {},
+		positionSectionTechnology : {},
+		positionSectionContact : {},
+		logoHeight: 0
+	};
+
+	document.addEventListener('DOMContentLoaded', startPage, false);
 	document.addEventListener('DOMContentLoaded', slowScroll, false);
+
+	function startPage() {
+		variables.sectionWhatDo = $("#whatDo");
+		variables.sectionaAboutUs = $("#aboutUs");
+		variables.sectiontTechnology = $("#technology");
+		variables.sectionContact = $("#contact");
+	}
 	
-  function slowScroll() {
-	  $(function() {
-		  $('a[href*="#"]:not([href="#"])').click(function() {
-		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-		      var target = $(this.hash);
-		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		      if (target.length) {
-		        $('html,body').stop().animate({
-		          scrollTop: target.offset().top - 44
-		        }, 1000);
-		        return false;
-		      }
-		    }
-		  });
+	function setSize() {
+		logoHeight = $(".logo").height()-1;
+		variables.positionSectionWhatDo = parseInt(variables.sectionWhatDo
+				.position().top - logoHeight);
+		
+		variables.positionSectionAboutUs = parseInt(variables.sectionaAboutUs
+				.position().top - logoHeight);
+		
+		variables.positionSectionTechnology = parseInt(variables.sectiontTechnology
+				.position().top - logoHeight);
+		
+		variables.positionSectionContact = parseInt(variables.sectionContact
+				.position().top - logoHeight);
+	}
+
+	function slowScroll() {
+		$(function() {
+			$('a[href*="#"]:not([href="#"])')
+					.click(
+							function() {
+								setSize();
+								if (location.pathname.replace(/^\//, '') == this.pathname
+										.replace(/^\//, '')
+										&& location.hostname == this.hostname) {
+									var target = $(this.hash);
+									target = target.length ? target
+											: $('[name=' + this.hash.slice(1)
+													+ ']');
+									if (target.length) {
+										$('html,body')
+												.stop()
+												.animate(
+														{
+															scrollTop : target
+																	.offset().top - logoHeight
+														}, 1000);
+										return false;
+									}
+								}
+							});
 		});
 	}
-}());
 
-var showLogoAnd = (function() {
-	var fadeSpeed = 200, fadeTo = 0.5, topDistance = 30;
-	var topbarME = function() { $('#nav').fadeTo(fadeSpeed,1); };
-	var topbarML = function() { $('#nav').fadeTo(fadeSpeed,fadeTo); };
-	var inside = false;
-	$(window).scroll(function() {
+	var show = (function() {
+		var topDistance = 30;
 		position = $(window).scrollTop();
-		if(position > topDistance && !inside) {
+		colorMenu(position);
+		showLogo(topDistance, position);
+		showIcons(topDistance, position);
+		$(window).scroll(function() {
+			position = $(window).scrollTop();
+			colorMenu(position);
+			showLogo(topDistance, position);
+			showIcons(topDistance, position);
+		});
+	})();
+
+	function showLogo(topDistance, position) {
+		if (position > topDistance) {
 			$('#logo').addClass("logoShow");
 			$('#menu').addClass("menuRight");
-		}
-		else if (position < topDistance){
+		} else if (position < topDistance) {
 			$('#logo').removeClass("logoShow");
 			$('#menu').removeClass("menuRight");
 		}
-		if(position>900){
+	}
+
+	function showIcons(topDistance, position) {
+		if (position > 900) {
 			$('.hideImg').addClass("showImg");
 		}
-	});
-})();
+	}
+
+	function colorMenu(position) {
+		if (position < (variables.positionSectionWhatDo + 2)
+				&& position > (variables.positionSectionWhatDo - 2)) {
+			$('.menuLinkWhatDo').addClass("menuLinkWhatDoNewColor");
+		} else {
+			$('.menuLinkWhatDo').removeClass("menuLinkWhatDoNewColor");
+		}
+
+		if (position < (variables.positionSectionAboutUs + 2)
+				&& position > (variables.positionSectionAboutUs - 2)) {
+			$('.menuLinkAboutUs').addClass("menuLinkAboutUsNewColor");
+		} else {
+			$('.menuLinkAboutUs').removeClass("menuLinkAboutUsNewColor");
+		}
+		console
+		if (position < (variables.positionSectionTechnology + 2)
+				&& position > (variables.positionSectionTechnology - 2)) {
+			$('.menuLinkTechnology').addClass("menuLinkTechnologyNewColor");
+		} else {
+			$('.menuLinkTechnology').removeClass("menuLinkTechnologyNewColor");
+		}
+		if (position < (variables.positionSectionContact + 2)
+				&& position > (variables.positionSectionContact - 2)) {
+			$('.menuLinkContact').addClass("menuLinkContactNewColor");
+		} else {
+			$('.menuLinkContact').removeClass("menuLinkContactNewColor");
+		}
+	}
+
+}());
